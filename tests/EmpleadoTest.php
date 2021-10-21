e3e819bcd8a20bf49b00b068a6726d89e35934fd
Matias Curto <62069934+matiascurto@users.noreply.github.com>
2021-10-20 22:14:41 -0300

    Agrega salario y excepción

--- tests/EmpleadoTest.php
+++ tests/EmpleadoTest.php
@@ -20,5 +20,34 @@ abstract class EmpleadoTest extends \PHPUnit\Framework\TestCase {
         $this->expectException(\Exception::class);
         $r = $this->crear("Matias" , "");
     }
+    public function testArrojaDNI()
+    {
+        $r = $this->crear(42482377);
+        $this->assertEquals(42482377, $r->getDNI());
+    }
+
+    public function testNoArrojaDNISiEstaVacio()
+    {
+        $this->expectException(\Exception::class);
+        $r = $this->crear("Fulano", "de tal", null, 9999);
+    }
+
+    public function testNoArrojaDNISiHayUnaLetra()
+    {
+        $this->expectException(\Exception::class);
+        $r = $this->crear("Fulano", "de tal", "a", 9999);
+    }
     
+    public function testArrojaSalario()
+    {
+        $r = $this->crear(1000);
+        $this->assertEquals(1000, $r->getSalario());
+    }
+
+    public function testNoArrojaSalario()
+    {
+        $this->expectException(\Exception::class);
+        $r = $this->crear("Fulano", "de tal", 9999, null);
+    }
+
 }
